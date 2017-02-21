cdb5c0dd82a0a45a2cdb932b1a881e491d388163
bhirendra <bhirendra2014@gmail.com>
2017-02-21 13:45:44 +0530

    Fix Issue in mysql status

--- navmo/urls.py
+++ navmo/urls.py
@@ -32,7 +32,7 @@ from register.views_results import spr_report,generate_marks
 from register.views_results import convert_to_pdf
 from register.views_results import send_email,register_raj
 from register.views_results import data_panel_login,data_panel_logout, data_panel_home
-from register.views_results import admin_results,scheduler
+from register.views_results import admin_results,scheduler,mysql_status
 
 
 urlpatterns = [
@@ -85,6 +85,7 @@ urlpatterns = [
     url(r'^data_panel_logout/$',data_panel_logout),
     url(r'^data_panel_home/$',data_panel_home),
     url(r'^scheduler/$',scheduler),
+    url(r'^mysql_status/$',mysql_status),
 ]
 from django.conf import settings
 from django.conf.urls.static import static
