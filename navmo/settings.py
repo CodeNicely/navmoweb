9a4753389598a1b4860d11cd4bf34cb402e54e3e
bhirendra <bhirendra2014@gmail.com>
2017-03-01 14:12:41 +0530

    Generate SPR of NH and MPE Raipur

--- navmo/settings.py
+++ navmo/settings.py
@@ -145,8 +145,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = "media/"
-MEDIA_ROOT = os.path.join(BASE_DIR,"/media/")
+MEDIA_ROOT = "media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR,"/media/")
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'noreplycodenicely@gmail.com'
